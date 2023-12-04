526cf3f3820dce6d8159c98c0cf89abd0868b78c
Alex <alexsmith11nd@gmail.com>
2023-12-04 00:09:26 +0000

    Refactored to remove redundant middle class layer

--- Fallout4CharacterGen/DoStuff.cs
+++ Fallout4CharacterGen/DoStuff.cs
@@ -12,44 +12,7 @@ namespace Fallout4CharacterGen
 {
     public class DoStuff
     {
-        private readonly ICsvJsonReaderWriter _readerWriter = new CsvAndJsonReaderWriter();
-        private readonly ICharacterBuilder _characterBuilder = new CharacterBuilder();
-
-        /// <summary>
-        /// This must be run once to populate the database.
-        ///
-        /// No longer needed as the data has been processed and is now hosted on my GitHub.
-        /// </summary>
-        /// <returns></returns>
-        public async Task LoadAndWriteData()
-        {
-            await _readerWriter.AskForFilename(); // Run the CSV & JSON reader writer program
-        }
         
-        /// <summary>
-        /// Generate a character and export it as a JSON file.
-        /// </summary>
-        /// <returns></returns>
-        public async Task CreateCharacter()
-        {
-            /*
-             * generate character and parse to json
-             *
-             * list:
-             * - Name                                          [DONE]
-             * - companion                                     [DONE]
-             * - settlements (where to consider 'home')        [DONE]
-             * - perks                                         [DONE]
-             * - disposition (unhinged, happy, sad, bad, etc)
-             * * - weapon / build (energy: laser, melee: blunt, etc)  TODO: generate a JSON of all weapons
-             * - faction
-             * - kleptomaniac (bool)
-             */
-
-            var character = await _characterBuilder.BuildCharacter();
-
-            // Export character Data for frontend:
-            JsonParser.WriteCharacterToDisk(character, character.Name);
-        }
+        
     }
 }
--- Fallout4CharacterGen/Program.cs
+++ Fallout4CharacterGen/Program.cs
@@ -1,15 +1,58 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Fallout4CharacterGen;
+using Fallout4CharacterGen.DatabaseSource.Local;
+using Fallout4CharacterGen.Interfaces;
+using Fallout4CharacterGen.Middleware;
 
 namespace Fallout4CharacterGen
 {
-    class Program
+    public class Program
     {
-        static async Task Main(string[] args)
+        private readonly ICsvJsonReaderWriter _readerWriter = new CsvAndJsonReaderWriter();
+        private readonly ICharacterBuilder _characterBuilder = new CharacterBuilder();
+
+        private static async Task Main(string[] args)
         {
-            var userInterface = new DoStuff();
+            var userInterface = new Program();
             await userInterface.CreateCharacter();
+        }
+        
+        /// <summary>
+        /// This must be run once to populate the database.
+        ///
+        /// No longer needed as the data has been processed and is now hosted on my GitHub.
+        /// </summary>
+        /// <returns></returns>
+        public async Task LoadAndWriteData()
+        {
+            await _readerWriter.AskForFilename(); // Run the CSV & JSON reader writer program
+        }
+        
+        /// <summary>
+        /// Generate a character and export it as a JSON file.
+        /// </summary>
+        /// <returns></returns>
+        private async Task CreateCharacter()
+        {
+            /*
+             * generate character and parse to json
+             *
+             * list:
+             * - Name                                          [DONE]
+             * - companion                                     [DONE]
+             * - settlements (where to consider 'home')        [DONE]
+             * - perks                                         [DONE]
+             * - disposition (unhinged, happy, sad, bad, etc)
+             * * - weapon / build (energy: laser, melee: blunt, etc)  TODO: generate a JSON of all weapons
+             * - faction
+             * - kleptomaniac (bool)
+             */
+
+            var character = await _characterBuilder.BuildCharacter();
+
+            // Export character Data for frontend:
+            JsonParser.WriteCharacterToDisk(character, character.Name);
         }
     }
 }
